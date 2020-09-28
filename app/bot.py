6e5ed68f1c3bd704cf19b69a42467ab98381b058
Mwiza Simbeye <mwiza@agripredict.com>
2020-09-28 20:52:25 +0200

    added basic profanity bot

--- app/bot.py
+++ app/bot.py
@@ -1,11 +1,21 @@
 import os
 import discord 
+import requests
 from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 class ProfanityBotClient(discord.Client):
 
+    def check_profanity(self, text):
+        url = "https://www.purgomalum.com/service/containsprofanity?text={}".format(text)
+
+        payload = {}
+        headers= {}
+
+        response = requests.request("GET", url, headers=headers, data = payload)
+
+        return (response.text)
 
     async def on_ready(self):
         print("Connected")
@@ -15,8 +25,12 @@ class ProfanityBotClient(discord.Client):
             return 
 
         channel = message.channel 
-        #
-        await channel.send("Test back")
+        profanity = self.check_profanity(message.content)
+        if profanity == 'true':
+            # Flag profanity and store into ProfDB
+            await channel.send("Profanity detected! Incident reported.")
+        else:
+            pass
 
 
 ProfanityBotClient().run(TOKEN)
