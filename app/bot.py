ad7d9a053748996617f20dbc93898328826d6742
Mwiza Simbeye <mwiza@agripredict.com>
2020-09-29 11:02:40 +0200

    Record offences in MongoDB

--- app/bot.py
+++ app/bot.py
@@ -13,10 +13,10 @@ class ProfanityBotClient(discord.Client):
         # establing connection
         try:
             client = MongoClient("mongodb://localhost:27017/")
-            print("Connected successfully!!!")
+            print("[INFO] Connected to MongoDB")
         except:
-            print("Could not connect to MongoDB")
-
+            print("[ERROR] Could not connect to MongoDB")
+ 
         # Check if database exists.
         dbnames = client.list_database_names()
         if "profantor" in dbnames:
@@ -25,9 +25,11 @@ class ProfanityBotClient(discord.Client):
 
             # creating or switching to demoCollection
             collection = db.offences
-            post = {"user_id": user_id, "name": name, "text": text, "offenses": 1}
-            record = collection.insert_one(post)
-            print(record)
+            if collection.count_documents({ 'user_id': user_id }, limit = 1) != 0:
+                collection.find_and_modify(query={'user_id':user_id}, update={"$inc": {'offenses': 1}}, upsert=False, full_response= True)
+            else:
+                post = {"user_id": user_id, "name": name, "offenses": 1}
+                record = collection.insert_one(post)
 
     def check_profanity(self, text):
         url = "https://www.purgomalum.com/service/containsprofanity?text={}".format(
@@ -42,7 +44,7 @@ class ProfanityBotClient(discord.Client):
         return response.text
 
     async def on_ready(self):
-        print("Connected")
+        print("[INFO] Connected to Discord")
 
     async def on_message(self, message):
         if message.author.id == self.user.id:
